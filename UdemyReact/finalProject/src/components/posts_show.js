2ce6ac491e5a8c8e688f0a67725fbcf3bf642a38
gaudy.blanco <gbm2494@hotmail.com>
2017-06-23 19:34:40 -0600

    final changes

--- UdemyReact/finalProject/src/components/posts_show.js
+++ UdemyReact/finalProject/src/components/posts_show.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost, deletePost} from '../actions';
+import {Link} from 'react-router-dom';
 
 class PostShow extends Component{
+  componentDidMount(){
+    //react router provide us params from the URL
+    const {id} = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  onDeleteClick(){
+    const {id} = this.props.match.params;
+    
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render(){
+    const {post} = this.props;
+
+    if(!post){
+        return <div>Loading...</div>;
+    }
+
     return(
       <div>
-        Post Show!
+        <Link to="/">Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
-export default PostShow;
+//ownProps == this.props
+function mapStateToProps({ posts }, ownProps){
+  return {post : posts[ownProps.match.params.id]};
+}
+
+//second param : receive action creator
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
--- UdemyReact/finalProject/src/reducers/reducer_posts.js
+++ UdemyReact/finalProject/src/reducers/reducer_posts.js
@@ -1,10 +1,21 @@
 //we don't need to specify any file because
 //the element is inside /actions/index.js
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action){
   switch(action.type){
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+    case FETCH_POST:
+      //ESX5 syntax
+      const post = action.payload.data;
+      const newState = {...state};
+      newState[post.id] = post;
+      return newState;
+
+      //ESX6 syntax
+      //return {...state, [action.payload.data.id] : action.payload.data}
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
